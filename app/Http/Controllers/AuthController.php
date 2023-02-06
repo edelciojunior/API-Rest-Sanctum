946c4849553798bbb1514e293896b4bc84193e01
Edélcio Júnior <junior@ibooking.com.br>
2023-02-06 17:01:35 -0300

    First commit: API com autenticação Sanctum

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    //Registrar um novo usuário
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        $token = $user->createToken('primeirotoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    /**
+     * Login do Usuário
+     */
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+        
+        //Verificar se e-mail informado existe
+        $user = User::where('email', $request->email)->first();
+
+        //Valida usuário e senha
+        if(!$user || !Hash::check($request->password, $user->password)){
+            return response([
+                'message' => 'Credenciais Inválidas!'
+            ], 401);
+        }
+
+        $token = $user->createToken('primeirotoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+    /**
+     * Logout do Usuário
+     */
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+        return [
+            'message' => 'Logout efetuado com sucesso e tokens do usuário excluídos'
+        ];
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/products', [ProductsController::Class, 'index']); //index -> função definida no controller
+Route::get('/products/{id}', [ProductsController::Class, 'show']);
+Route::get('/products/search/{name}', [ProductsController::Class, 'search']);
+Route::post('/register', [AuthController::Class, 'register']);
+Route::post('/login', [AuthController::Class, 'login']);
+Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/{id}', [UserController::class, 'show']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/products', [ProductsController::Class, 'store']);
+    Route::put('/products/{id}', [ProductsController::Class, 'update']);
+    Route::delete('/products/{id}', [ProductsController::Class, 'destroy']);
+    Route::post('/logout', [AuthController::Class, 'logout']);
+});
+
+/*  Route::group(['middleware' => ['auth:sanctum']], function() {
+    Route::post('/products', [ProductsController::Class, 'store']);
+    Route::put('/products/{id}', [ProductsController::Class, 'update']);
+    Route::delete('/products/{id}', [ProductsController::Class, 'destroy']);
+}); */
+
+/* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+}); */
